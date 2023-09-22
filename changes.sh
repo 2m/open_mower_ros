9f580752caaaf3ce47480ae6e04a0dce3e078076
Martynas Mickevičius <self@2m.lt>
2023-09-22 17:49:00 +0300

    Changes to merge into release

---
+++ changes.sh
@@ -0,0 +1,4 @@
+# "Start in area" service implementation #66
+# https://github.com/ClemensElflein/open_mower_ros/pull/66
+# rebased on top of latest main in my branch
+git cherry-pick ..start_in_area_service
